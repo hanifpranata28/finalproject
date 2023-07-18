8e42f72ee9308a11d0981bb2db1c1429900575fd
anindyaha <anindyaha.official@gmail.com>
2023-07-19 01:25:21 +0700

    edit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        User::truncate();
+        Schema::enableForeignkeyConstraints();
+
+        User::insert([
+            'username' => 'Joni',
+            'password' => Hash::make('password'),
+            'phone' => 25,
+            'address' => 'Jl. fhsgfh',
+            'role_id' => '1'
+        ]);
+    }
+}
